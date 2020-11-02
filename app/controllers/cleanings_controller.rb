b4fee390372820f9eea804319adcc1c01f4c0106
M.Ishii <t10801606@gunma-u.ac.jp>
2020-11-02 18:21:28 +0900

    トップページの準備

---
+++ app/controllers/cleanings_controller.rb
@@ -0,0 +1,4 @@
+class CleaningsController < ApplicationController
+  def index
+  end
+end
---
+++ test/controllers/cleanings_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class CleaningsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get cleanings_index_url
+    assert_response :success
+  end
+
+end
