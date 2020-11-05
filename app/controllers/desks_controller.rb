4a9edd700a6d9b49070a595c05776d48ed2325b0
M.Ishii <t10801606@gunma-u.ac.jp>
2020-11-05 18:10:22 +0900

    画像投稿機能の実装

--- app/controllers/desks_controller.rb
+++ app/controllers/desks_controller.rb
@@ -1,6 +1,26 @@
 class DesksController < ApplicationController
+  before_action :authenticate_user!, except: :index
   def index
     @desks = Desk.includes(:user).order("created_at DESC")
   end
   
+  def new
+    @desk = Desk.new
+  end
+
+  def create
+    @desk = Desk.new(desk_params)
+    if @desk.save
+      redirect_to desks_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def desk_params
+    params.require(:desk).permit(:title, :concept, :image).merge(user_id: current_user.id)
+  end
+
 end
