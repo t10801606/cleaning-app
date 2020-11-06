575431e712c025eb08b0eca52d430896f77037f3
M.Ishii <t10801606@gunma-u.ac.jp>
2020-11-06 14:50:56 +0900

    Userモデルのテストコード実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name {Faker::Name.name}
-    email {Faker::Internet.free_email}
-    password = { 'test1234TEST'}
-    password_confirmation {password}
+    nickname                { Faker::Name.name }
+    email                   { Faker::Internet.free_email }
+    password                { 'test1234TEST' }
+    password_confirmation   { password }
   end
 end
