6b7c7f62a7d332130d7cbbe351b57bdfc3cd1df1
M.Ishii <t10801606@gunma-u.ac.jp>
2020-11-11 16:41:19 +0900

    掃除情報の保存機能実装(createアクション)

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -5,4 +5,29 @@ class SuggestionsController < ApplicationController
   def new
     @suggestion = Suggestion.new
   end
+
+  def create
+    @suggestion = Suggestion.new(suggestion_params)
+    status_judge
+    if @suggestion.save
+      redirect_to suggestions_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def suggestion_params
+    params.require(:suggestion).permit(:place, :period_cleaning, :last_cleaned_date, :status).merge(user_id: current_user.id)
+  end
+
+  def status_judge
+    this_day = Date.today
+    num_days = (this_day - @suggestion.last_cleaned_date).to_i
+    if  @suggestion.period_cleaning <= num_days
+      @suggestion.status = false
+    else
+      @suggestion.status = true
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :suggestions, only: [:index, :new]
   devise_for :users
   root to: "cleanings#index"  
   resources :desks do
     resources :comments, only: :create
   end 
+  resources :suggestions, only: [:index, :new, :create]
 end
